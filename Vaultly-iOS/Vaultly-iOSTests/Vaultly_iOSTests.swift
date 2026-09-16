d3b651ff3bb5d192542a6ce70c13fd03f6404044
Alicank <alican.kurt.p@gmail.com>
2026-09-16 15:26:57 +0300

    Xcode Project Initial Commit

---
+++ Vaultly-iOS/Vaultly-iOSTests/Vaultly_iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  Vaultly_iOSTests.swift
+//  Vaultly-iOSTests
+//
+//  Created by Alicank on 16.09.2026.
+//
+
+import Testing
+@testable import Vaultly_iOS
+
+struct Vaultly_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
